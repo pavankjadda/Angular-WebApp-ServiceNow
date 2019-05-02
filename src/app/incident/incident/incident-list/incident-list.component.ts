8f1c922d695f110f2ba0dcf7f92ecf3d0531509d
Pavan Jadda <>
2019-05-02 18:02:06 -0400

    Updated CORS config

--- src/app/incident/incident/incident-list/incident-list.component.ts
+++ src/app/incident/incident/incident-list/incident-list.component.ts
@@ -1,15 +1,24 @@
 import {Component, OnInit} from '@angular/core';
+import {IncidentService} from '../service/incident.service';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-incident-list',
   templateUrl: './incident-list.component.html',
   styleUrls: ['./incident-list.component.css']
 })
-export class IncidentListComponent implements OnInit {
+export class IncidentListComponent implements OnInit
+{
 
-  constructor() { }
+  constructor(private incidentService:IncidentService,
+              private router:Router)
+  {
 
-  ngOnInit() {
+  }
+
+  ngOnInit()
+  {
+
   }
 
 }
