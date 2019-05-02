20a19d7f6bfc86ca42418ce67eb0bdf42d254087
Pavan Jadda <>
2019-05-02 15:15:12 -0400

    Updated Angular version

--- src/app/app.constants.ts
+++ src/app/app.constants.ts
@@ -1,7 +1,7 @@
-export const SERVER_API_URL = "http://localhost:8090/";
+export const SERVER_API_URL = 'https://dev81909.service-now.com/';
 export const BASIC_AUTH = btoa('admin:admin');
 
-export const OAUTH2_CLIENT_ID= 'spring-security-oauth2-read-write-client';
-export const OAUTH2_CLIENT_SECRET= 'spring-security-oauth2-read-write-client-password1234';
-export const OAUTH2_ACCESS_TOKEN_URI = SERVER_API_URL + "oauth/token";
-export const OAUTH2_USER_AUTHORIZATION_URI = SERVER_API_URL + "oauth/authorize";
+export const OAUTH2_CLIENT_ID= '3c76622bc581b30082098914f97ee08e';
+export const OAUTH2_CLIENT_SECRET= 'bcmc12345';
+export const OAUTH2_ACCESS_TOKEN_URI = SERVER_API_URL + 'oauth_token.do';
+export const OAUTH2_USER_AUTHORIZATION_URI = SERVER_API_URL + 'oauth/authorize';
