40f9ea04279cd0b30b57ae82d01ff6f89708e24b
Pavan Jadda <jpavanaryan@gmail.com>
2020-03-13 13:42:30 -0400

    Upgrade to Angular 9

--- src/app/incident/incident.module.ts
+++ src/app/incident/incident.module.ts
@@ -1,16 +1,15 @@
-import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
-import {IncidentComponent} from './incident/incident.component';
-import {IncidentListComponent} from './incident/incident-list/incident-list.component';
-import {IncidentHomeComponent} from './incident/incident-home/incident-home.component';
-import {IncidentEditComponent} from './incident/incident-edit/incident-edit.component';
-import {IncidentViewComponent} from './incident/incident-view/incident-view.component';
-import {IncidentNewComponent} from './incident/incident-new/incident-new.component';
-import {IncidentRoutingModule} from './incident-routing.module';
-import {TableModule} from 'primeng/table';
-import {DropdownModule, InputTextModule} from 'primeng/primeng';
+import {NgModule} from '@angular/core';
 import {NgxSpinnerModule} from 'ngx-spinner';
+import {DropdownModule, InputTextModule, TableModule} from 'primeng';
+import {IncidentRoutingModule} from './incident-routing.module';
 import {IncidentTypeComponent} from './incident-type/incident-type.component';
+import {IncidentEditComponent} from './incident/incident-edit/incident-edit.component';
+import {IncidentHomeComponent} from './incident/incident-home/incident-home.component';
+import {IncidentListComponent} from './incident/incident-list/incident-list.component';
+import {IncidentNewComponent} from './incident/incident-new/incident-new.component';
+import {IncidentViewComponent} from './incident/incident-view/incident-view.component';
+import {IncidentComponent} from './incident/incident.component';
 
 @NgModule({
   declarations: [
