871f3c382c05cf94d51cf4461e9eff6f5c82c714
Pavan Jadda <pavankjadda@users.noreply.github.com>
2022-02-10 19:16:18 -0500

    chore: updates instance config

--- src/app/app.constants.ts
+++ src/app/app.constants.ts
@@ -1,8 +1,8 @@
-export const SERVER_API_URL = 'https://dev69335.service-now.com/';
-export const USER_INFO_URL = '/api/19668/angulartest';
+export const SERVER_API_URL = 'https://dev89855.service-now.com/';
+export const USER_INFO_URL = '/api/x_19668_test/getuserdetails';
 
-export const OAUTH2_CLIENT_ID= 'a4302d25aa8b30102f4bb051ac2ab1a0';
-export const OAUTH2_CLIENT_SECRET= 'Test@12345';
+export const OAUTH2_CLIENT_ID= '3e3d87d601a501107d5aadce8576d935';
+export const OAUTH2_CLIENT_SECRET= 'F:rd{]0Sp&';
 export const OAUTH2_ACCESS_TOKEN_URI = SERVER_API_URL + 'oauth_token.do';
 
 export const INCIDENT_API_URL = 'api/now/table/incident?sysparm_display_value=true';
